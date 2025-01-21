19594aada1f041ec881ad6002754f68c6ef50354
Rayane Lamlaika <lamlaikarayane@gmail.com>
2025-01-21 08:27:44 +0100

    dsds

---
+++ ft_split.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 04:51:14 by rlamlaik          #+#    #+#             */
+/*   Updated: 2025/01/20 10:28:40 by rlamlaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static size_t	count_word(char const *str, char c)
+{
+	size_t	i;
+	size_t	o;
+
+	o = 0;
+	i = 0;
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str != c && o == 0)
+		{
+			o = 1;
+			i++;
+		}
+		else if (*str == c)
+			o = 0;
+		str++;
+	}
+	return (i);
+}
+
+static size_t	lenght(char const *str, char c)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (*str && *str != c)
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+static char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!src || !dest)
+		return (0);
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+static char	**intotowdarray(char const*s, char c, char**p, size_t*index)
+{
+	size_t	words;
+
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s != c && *s != '\0')
+		{
+			words = lenght(s, c);
+			p[*index] = (char *) malloc(sizeof(char) * (words + 1));
+			if (!p)
+			{
+				while ((*index) > 0)
+					free(p[(*index)--]);
+				free (p);
+				return (0);
+			}
+			ft_strncpy(p[*index], s, words);
+			p[*index][words] = '\0';
+			(*index)++;
+			s += words;
+		}
+	}
+	return (p);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	words;
+	char	**p;
+	size_t	index;
+
+	if (!s)
+		return (NULL);
+	words = count_word(s, c);
+	p = (char **) malloc(sizeof(char *) * (1 + words));
+	if (!p)
+		return (NULL);
+	index = 0;
+	p = intotowdarray(s, c, p, &index);
+	if (!p)
+		return (NULL);
+	p[index] = NULL;
+	return (p);
+}
--- main.c
+++ main.c
@@ -1,44 +1,113 @@
-/*
-   ----Pipex----
-
-
-	-First let's start with run in multiple processes
-	with using fork, dup and maybe pipe.
-(-ps -ef --forest) withis command we can see all prcesses runing on your pc
-*/
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <fcntl.h>
-// First take the parametaer 'file first and the last'
-//applay the first command on the file 1 
-//copy the output into other file with dump2()
-//pass the output of the first command to the second command 
-//push the output of that to the last file in argemmnet
+#include "pipex.h"
 
-int main(int ac, char **av)//, char **ev)
+// char **searsh_path(char **ev)
+// {
+// 	char  **thepath;
+// 	int   i;
+
+// 	i = 0;
+// 	char *ii = ev[76] + 5;
+// 	printf("%s\n", ii);
+// 	thepath = ft_split(ii, ':');
+// 	return (thepath);
+// }
+
+// int main(int ac, char **av, char **ev)
+// {
+// 	int i;
+// 	int pipefd[2];
+// 	int pid;
+
+// 	char *cat[]={"/bin/cat", "file1", NULL};
+// 	pipe(pipefd);
+
+// 	if (pid == 0)
+// 	{
+
+
+
+// 	}
+
+
+	// if (ac == 5)
+	// {
+	// 	i = open(av[1], O_RDONLY, 0777);
+	// 	if (i < 0)
+	// 		return (write(2 ,"mochkil f file1", 16), 0);
+	// 	if (fork() == 0)
+	// 	{
+	// 		if (execve(NULL,NULL, NULL)== -1)
+	// 			return (write(1, "execve failed", 14), 0);
+
+
+
+	// 	}
+
+// 	}
+// }
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int ac, char **av, char **ev)
 {
-	int	i;
-	int	j;
+// first work with access to check if the path are exec
+	if (access("/bin/cat", X_OK) == 0)
+		return (write(1, "ra 9raa 9ra \n", 14), 0);
+	else
+		return (write(1, "khadamach", 10), 0);
 
-	i = 1;
-	char *file1;
-	char *file2;
-	char *txt;
+thisis s
 
 
-	if (ac == 5)
-	{
-		file1 = av[1];
-		file2 = av[ac - 1];
-		j = open(file1, O_RDWR, 0777);
-		if (j == -1)
-			return (write(1, "prblm in open", 14), 0);
-		read(j,txt,4);
-		write(1, txt,4);
 
-		//everthing are between those files are command "mindatory only 2 command"
-
-	}
 }
+
+
+/*
+1. Vérifier les arguments d'entrée
+	- Si le nombre d'arguments est incorrect, afficher une erreur et quitter.
+
+2. Ouvrir file1 en lecture
+	- Si l'ouverture échoue, afficher une erreur et quitter.
+
+3. Ouvrir file2 en écriture (créer ou tronquer)
+	- Si l'ouverture échoue, afficher une erreur et quitter.
+
+4. Créer un pipe
+	- Si la création échoue, afficher une erreur et quitter.
+
+5. Fork pour le premier processus (cmd1)
+	- Dans le processus enfant :
+		 - Rediriger l'entrée vers file1.
+		 - Rediriger la sortie vers l'extrémité d'écriture du pipe.
+		 - Fermer les extrémités inutilisées du pipe.
+		 - Exécuter cmd1 avec execve.
+	- Dans le processus parent :
+		 - Fermer l'extrémité d'écriture du pipe.
+
+6. Fork pour le second processus (cmd2)
+	- Dans le processus enfant :
+		 - Rediriger l'entrée vers l'extrémité de lecture du pipe.
+		 - Rediriger la sortie vers file2.
+		 - Fermer les extrémités inutilisées du pipe.
+		 - Exécuter cmd2 avec execve.
+	- Dans le processus parent :
+		 - Fermer l'extrémité de lecture du pipe.
+
+7. Attendre que les processus enfants se terminent.
+
+8. Fermer les fichiers ouverts.
+
+9. Quitter le programme.
+*/
