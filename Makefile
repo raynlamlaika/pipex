30bf34198073cf0f2aaca1a5f61a0dabf26b5e1d
Rayane Lamlaika <lamlaikarayane@gmail.com>
2025-01-27 13:24:21 +0100

    norm

--- Makefile
+++ Makefile
@@ -1,1 +1,15 @@
-NAME= pipex
+NAME=pipex
+SRC= pipex.c help.c ft_strjoin.c ft_split.c
+OBJ=$(SRC:.c=.o)
+CC=cc
+CFLAGS=-Wall -Werror -Wextra
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $^ -o $(NAME)
+clean:
+	rm -f $(OBJ)
+re : clean all
+fclean: clean
+	rm -f $(NAME) 
--- pipex.c
+++ pipex.c
@@ -6,13 +6,13 @@
 /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 10:49:31 by rlamlaik          #+#    #+#             */
-/*   Updated: 2025/01/26 21:21:11 by rlamlaik         ###   ########.fr       */
+/*   Updated: 2025/01/27 13:23:54 by rlamlaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**takepaths(char** env)
+char	**takepaths(char **env)
 {
 	char	*pathstr;
 	char	**back;
@@ -21,7 +21,6 @@ char	**takepaths(char** env)
 	if (!env)
 		exit(EXIT_FAILURE);
 	enc = 0;
-	int i = 0;
 	while (env[enc])
 	{
 		if (ft_strncmp("PATH=", env[enc], 5) == 0)
@@ -35,31 +34,29 @@ char	**takepaths(char** env)
 	back = ft_split(pathstr, ':');
 	while (back[enc])
 	{
-		back[enc] = ft_strjoin(back[enc] , "/");
+		back[enc] = ft_strjoin(back[enc], "/");
 		enc++;
 	}
-	return(back);
+	return (back);
 }
 
-char	*pick(char **path,char* cmd)
+char	*pick(char**path, char*cmd)
 {
 	int		pass;
 	char	*realpath;
 
 	pass = 0;
-	
 	if (ft_strrchr(cmd, '/'))
 	{
 		if (access(cmd, X_OK) == 0)
-			return(cmd);
+			return (cmd);
 		return (NULL);
 	}
 	while (path[pass])
 	{
-		
 		realpath = ft_strjoin(path[pass], cmd);
 		if (access(realpath, X_OK) == 0)
-			return(realpath);
+			return (realpath);
 		free(realpath);
 		pass++;
 	}
@@ -75,7 +72,8 @@ void	firstcmd(char **av, char **paths, int *pipfd)
 	infile = open(av[1], O_RDONLY);
 	if (infile < 0)
 		return ;
-	if (dup2(infile, STDIN_FILENO) == -1 || dup2(pipfd[1], STDOUT_FILENO) == -1)
+	if (dup2(infile, STDIN_FILENO) == -1 || \
+	dup2(pipfd[1], STDOUT_FILENO) == -1)
 		return ;
 	cmd = ft_split(av[2], ' ');
 	path = pick(paths, cmd[0]);
@@ -86,13 +84,14 @@ void	firstcmd(char **av, char **paths, int *pipfd)
 	return ;
 }
 
-int main (int ac, char **av, char **ev)
+int	main(int ac, char**av, char**ev)
 {
 	int		pipfd[2];
 	pid_t	pid;
 	char	**paths;
 	char	*path;
 	int		outfile;
+	char	**cmd;
 
 	if (ac != 5)
 		exit(EXIT_FAILURE);
@@ -102,17 +101,18 @@ int main (int ac, char **av, char **ev)
 	pid = fork();
 	if (pid == -1)
 		return (write(2, "fork failed\n", 13), 0);
-	if(pid == 0)
+	if (pid == 0)
 		firstcmd(av, paths, pipfd);
 	else if (fork() == 0)
 	{
 		outfile = open(av[4], O_CREAT | O_TRUNC | O_APPEND | O_RDWR, 0644);
 		if (outfile == -1)
 			return (write(1, "open fAiled 1\n", 15), 0);
-		if (dup2(pipfd[0], STDIN_FILENO) == -1 ||dup2(outfile, STDOUT_FILENO) == -1)
+		if ((dup2(pipfd[0], STDIN_FILENO) == -1) || \
+		(dup2(outfile, STDOUT_FILENO) == -1))
 			return (0);
 		close(pipfd[1]);
-		char **cmd= ft_split(av[3], ' ');
+		cmd = ft_split(av[3], ' ');
 		path = pick(paths, cmd[0]);
 		if (!path)
 			return (write(2, "path not reached\n", 18), 0);
@@ -122,7 +122,5 @@ int main (int ac, char **av, char **ev)
 	close(pipfd[0]);
 	close(pipfd[1]);
 	wait(NULL);
-	wait(NULL);
 	return (0);
 }
-
