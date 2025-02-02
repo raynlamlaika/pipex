022a8106a0f2f91c41e2474ad953a9463cb68895
Rayane Lamlaika <lamlaikarayane@gmail.com>
2025-02-02 22:19:33 +0100

    pipex khadmaa mazyaaaaaan

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 13:13:22 by rlamlaik          #+#    #+#             */
-/*   Updated: 2025/02/02 16:28:47 by rlamlaik         ###   ########.fr       */
+/*   Updated: 2025/02/02 22:18:58 by rlamlaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,22 @@ int	executing(int prev_pipe, char** av, int j, char **paths, int outfile)
 	char	**command;
 	char	*path;
 
-	if(fork() == 0)
+	if (fork() == 0)
 	{
 		if ((dup2(prev_pipe, STDIN_FILENO) == -1) || \
 			(dup2(outfile, STDOUT_FILENO) == -1))
-			return (write(2, "dup2 failed\n", 12), 0);
+			return (perror("dup2 failed"), 0);
+		close(prev_pipe);
 		close(outfile);
 		command = split(av[j]);
-		path = pick(paths, av[j]);
-		execve(path, command, NULL);
-		exit(1);
+		path = pick(paths, command[0]);
+		if (execve(path, command, NULL) == -1)
+		{
+			perror("execve failed");
+			exit(1);
+		}
 	}
-	else
-		return (0);
+	return (0);
 }
 
 int main(int ac, char **av, char **ev)
@@ -59,42 +62,58 @@ int main(int ac, char **av, char **ev)
 	int		i;
 	int		prev_pipe;
 
+	if (ac < 5)
+		return (write(2, "args not enough\n", 16), 0);
 
 	if (pipe(pipefd) == -1)
-		return (write(2, "pipe filed\n", 12), 0);
-	if (ac < 5)
-		return(write(2, "args not enogh\n", 16), 0);
+		return (write(2, "pipe failed\n", 12), 0);
+
 	paths = takepaths(ev);
 	if (!paths)
-		return(write(2, "path error\n", 12), 0);
+		return (write(2, "path error\n", 12), 0);
+
 	int infile = open(av[1], O_RDONLY);
-	int outfile = open(av[(ac - 1)], O_CREAT | O_TRUNC | O_APPEND | O_RDWR, 0644);
+	int outfile = open(av[ac - 1], O_CREAT | O_TRUNC | O_RDWR, 0644);
 	if (infile == -1 || outfile == -1)
-		return (write(2, "no infile/outfile of waht \n", 27), 0);
+		return (write(2, "file error\n", 12), 0);
 	i = 1;
-    while (i < ac - 2)
-    {
-		if (i == 1)
-			if (fork() == 0)
-			{
-				first_command(av, paths, pipefd);
-				prev_pipe = pipefd[0];
-			}
-		else 
+	while (i < ac - 2)
+	{
+		if (pipe(pipefd) == -1)
+			return (write(2, "pipe failed\n", 12), 0);
+		pid_t pid = fork();
+		if (pid == -1)
+			return (write(2, "fork failed\n", 12), 0);
+		if (pid == 0)
 		{
-			if (pipe(pipefd) == -1)
-				return (write(2, "pipe failed\n", 12), 0);
-			executing(prev_pipe, av, i, paths, pipefd[1]);
-			fprintf(stderr, "thisis the av file || cnmd [%s] and [%d] \n", av[i], i);
+			if (i == 1)
+				first_command(av, paths, pipefd);
+			else
+				executing(prev_pipe, av, i, paths, pipefd[1]);
+
+			close(pipefd[0]);
+			exit(0);
+		}
+		else
+		{
+			close(pipefd[1]);
+			if (i > 1)
+				close(prev_pipe);
+
+			prev_pipe = pipefd[0];
 		}
 		i++;
 	}
-    executing(prev_pipe, av, i, paths, outfile);
-    close(prev_pipe);
-    close(outfile);
-    wait(NULL);
-    return (0);
+
+	executing(prev_pipe, av, i, paths, outfile);
+
+	close(prev_pipe);
+	close(outfile);
+	while (wait(NULL) > 0);
+
+	return (0);
 }
+
 
 
 //finish pipex 
